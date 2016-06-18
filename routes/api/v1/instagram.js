ed0fbdba6b5660fc980cf7ced8f9bd0e88e0b6a0
Benjamin Knox <benjamin@clearvoice.com>
2016-06-18 08:41:26 -0700

    Add Instagram API calls

--- routes/api/v1/instagram.js
+++ routes/api/v1/instagram.js
@@ -1,11 +1,17 @@
+var ig = require('instagram-node').instagram();
 var express = require('express');
 var router = express.Router();
 
+ig.use({access_token: process.env['INSTAGRAM_TOKEN']});
 
 /* GET home page. */
 router.get('/instagram', function(req, res, next) {
-  res.send({
-    'api': 'Will give me my instagram posts'
+
+  var count = 10,
+      options = { count: req.params.pageSize || count };
+
+  ig.user_self_media_recent(options, function(err, medias){
+    res.send(medias);
   });
 });
 

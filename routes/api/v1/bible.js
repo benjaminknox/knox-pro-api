650eab7acaf1a9589b0385dad676bab050cff341
Benjamin Knox <benjamin@clearvoice.com>
2016-06-19 12:18:07 -0700

    Add /bible/daily_verse using esvapi.org to API

--- routes/api/v1/bible.js
+++ routes/api/v1/bible.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
-router.get('/bible', function(req, res, next) {
-  res.send({
-    'api': 'Will give me bible verses'
-  });
+router.get('/bible/daily_verse', function(req, res, next) {
+  request('http://www.esvapi.org/v2/rest/verse?&seed=7107&key=' + process.env['ESV_API_TOKEN'],
+    function (error, response, body) {
+      if (!error && response.statusCode == 200) {
+        res.send({
+          verseHtml: body
+        });
+      } else {
+        res.send(error);
+      }
+    }
+  );
 });
 
 module.exports = router;

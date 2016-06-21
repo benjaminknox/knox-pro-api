437d072fbe7ea417c545fa4dc2eb2cb104ca3abd
Benjamin Knox <benjamin@clearvoice.com>
2016-06-21 06:18:54 -0700

    Remove seed from daily_verse

--- routes/api/v1/bible.js
+++ routes/api/v1/bible.js
@@ -4,7 +4,7 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/bible/daily_verse', function(req, res, next) {
-  request('http://www.esvapi.org/v2/rest/verse?&seed=7107&key=' + process.env['ESV_API_TOKEN'],
+  request('http://www.esvapi.org/v2/rest/verse?&key=' + process.env['ESV_API_TOKEN'],
     function (error, response, body) {
       if (!error && response.statusCode == 200) {
         res.send({
